a019dcde751e3810716d63b0543f8c049b8275b5
naredo <nachonaredobernardos@gmail.com>
2024-11-12 17:34:07 +0100

    fix: Fix models.py

--- app/modules/fakenodo/repositories.py
+++ app/modules/fakenodo/repositories.py
@@ -1,10 +1,10 @@
-from app.modules.fakenodo.models import Fakenodo
+from app.modules.fakenodo.models import Deposition
 from core.repositories.BaseRepository import BaseRepository
 
 
 class FakenodoRepository(BaseRepository):
     def __init__(self):
-        super().__init__(Fakenodo)
+        super().__init__(Deposition)
 
         def create_new_deposition(self, dep_metadata):
             return self.create(dep_metadata=dep_metadata)
