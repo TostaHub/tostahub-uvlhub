4731ca250fd6c0a0681be060826f91033fe819e5
Sergio Aguayo <ahydul1@gmail.com>
2024-11-23 14:07:45 +0100

    feat: Add filter by valid uvl model and by range of numbers of configurations

--- app/modules/explore/services.py
+++ app/modules/explore/services.py
@@ -6,6 +6,9 @@ class ExploreService:
         self.repository = ExploreRepository()
 
     def filter(self, query: str, sorting="newest", publication_type="any",
-               start_date="", end_date="", min_uvl="", max_uvl="", **kwargs):
+               start_date="", end_date="", min_uvl="", max_uvl="", by_valid_uvls="",
+               min_num_configurations="", max_num_configurations="", **kwargs):
+
         return self.repository.filter(query, sorting, publication_type, start_date,
-                                      end_date, min_uvl, max_uvl, **kwargs)
+                                      end_date, min_uvl, max_uvl, by_valid_uvls, min_num_configurations,
+                                      max_num_configurations, **kwargs)
