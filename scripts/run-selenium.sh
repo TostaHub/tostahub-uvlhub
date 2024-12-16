e4cbd1164b7a924811ae8217b7b5033a1177cce1
Sergio Aguayo <ahydul1@gmail.com>
2024-12-16 17:54:37 +0100

    feat: Add missing selenium tests to run-selenium.sh

--- scripts/run-selenium.sh
+++ scripts/run-selenium.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Los siguientes modules los ignoramos porque tienen tests vacios: fakenodo webhook featuremodel flamapy hubfile
 #EL reset de la BD y El orden es importante porque hay algun test que modifica BD 
-rosemary db:reset -y && rosemary db:seed && rosemary selenium auth && rosemary selenium explore && rosemary selenium dataset && python app/modules/dataset/tests/test_selenium2.py && python app/modules/dataset/tests/test_selenium3.py
+rosemary db:reset -y && rosemary db:seed && rosemary selenium auth && rosemary selenium explore && pytest app/modules/dataset/tests/test_selenium3.py && rosemary selenium dataset && pytest app/modules/dataset/tests/test_selenium2.py
